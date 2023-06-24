55bfb67e94c07b519e205ef095d3547ea51709f3
stoiandelev <stoian.delev@yahoo.com>
2023-06-24 14:03:18 +0300

    _12_ValidParentheses with Stack

---
+++ src/main/java/Problems/Easy/_12_ValidParentheses.java
@@ -0,0 +1,47 @@
+package Problems.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ * An input string is valid if:
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ */
+
+public class _12_ValidParentheses {
+    public static void main(String[] args) {
+        String string = "()[]{}";
+        System.out.println(isValid(string));
+    }
+
+
+    public static boolean isValid(String string) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : string.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+
+                if (stack.empty()) {
+                    return false;
+                }
+
+                if (c == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else if (c == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else if (c == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
