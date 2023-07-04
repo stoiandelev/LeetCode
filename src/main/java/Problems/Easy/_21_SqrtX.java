13289714e158194f2dbae99014ff3acf5d7d78c4
stoiandelev <stoian.delev@yahoo.com>
2023-07-04 22:20:15 +0300

    SqrtX

---
+++ src/main/java/Problems/Easy/_21_SqrtX.java
@@ -0,0 +1,36 @@
+package Problems.Easy;
+
+/**
+ * Given a non-negative integer x, return the square root of x
+ * rounded down to the nearest integer. The returned integer should be non-negative as well.
+ * You must not use any built-in exponent function or operator.
+ */
+
+public class _21_SqrtX {
+    public static void main(String[] args) {
+        int number = 8;
+
+        System.out.println(mySqrt(number));
+    }
+
+    public static int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+
+        int first = 1;
+        int last = x;
+
+        while (first <= last) {
+            int mid = first + (last - first) / 2;
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid > x / mid) {
+                last = mid - 1;
+            } else {
+                first = mid + 1;
+            }
+        }
+        return last;
+    }
+}
