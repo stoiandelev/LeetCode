ba5a6c67e37f9bedfb846df3ef29bfeeb1a99dce
stoiandelev <stoian.delev@yahoo.com>
2023-07-12 12:45:24 +0300

    Tree traversal and saved in List

---
+++ src/main/java/Problems/Easy/_25_BinaryTreeInorderTraversal.java
@@ -0,0 +1,70 @@
+package Problems.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _25_BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+
+
+    }
+
+    List<Integer> ans = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return ans;
+        }
+        inorderTraversal(root.left);
+        ans.add(root.val);
+        inorderTraversal(root.right);
+        return ans;
+    }
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public int getVal() {
+            return val;
+        }
+
+        public TreeNode setVal(int val) {
+            this.val = val;
+            return this;
+        }
+
+        public TreeNode getLeft() {
+            return left;
+        }
+
+        public TreeNode setLeft(TreeNode left) {
+            this.left = left;
+            return this;
+        }
+
+        public TreeNode getRight() {
+            return right;
+        }
+
+        public TreeNode setRight(TreeNode right) {
+            this.right = right;
+            return this;
+        }
+    }
+}
