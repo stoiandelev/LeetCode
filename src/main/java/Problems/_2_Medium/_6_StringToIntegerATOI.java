d69318b83f659fcc8a370c20971effbd9a51bc5b
stoiandelev <stoian.delev@yahoo.com>
2023-08-17 15:11:27 +0300

    String to Integer ATOI approach

---
+++ src/main/java/Problems/_2_Medium/_6_StringToIntegerATOI.java
@@ -0,0 +1,65 @@
+package Problems._2_Medium;
+
+import java.math.BigInteger;
+
+
+/**
+ * Implement the myAtoi(string s) function,
+ * which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+ * The algorithm for myAtoi(string s) is as follows:
+ * Read in and ignore any leading space.
+ * Check if the next character (if not already at the end of the string) is '-' or '+'.
+ * Read this character in if it is either.
+ * This determines if the final result is negative or positive respectively.
+ * Assume the result is positive if neither is present.
+ * Read in next the characters until the next non-digit
+ * character or the end of the input is reached. The rest of the string is ignored.
+ * Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32).
+ * If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+ * If the integer is out of the 32-bit signed integer range [-231, 231 - 1],
+ * then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+ * Return the integer as the final result.
+ */
+
+public class _6_StringToIntegerATOI {
+    public static void main(String[] args) {
+
+        String s = "   -42";
+        System.out.println(myAtoi(s));
+
+    }
+
+
+    public static int myAtoi(String s) {
+        s = s.trim();
+        StringBuilder sb = new StringBuilder();
+        char[] arr = s.toCharArray();
+
+        if (s.isEmpty()) {
+            return 0;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (Character.isDigit(arr[i]) || (i == 0 && (arr[i] == '-' || arr[i] == '+'))) {
+                sb.append(arr[i]);
+            } else {
+                break;
+            }
+        }
+
+        if (sb.isEmpty() || (sb.length() == 1 && (sb.charAt(0) == '-' || sb.charAt(0) == '+'))) {
+            return 0;
+        }
+
+        BigInteger result = new BigInteger(sb.toString());
+
+        if (result.compareTo(BigInteger.valueOf(Integer.MAX_VALUE)) > 0) {
+            return Integer.MAX_VALUE;
+        } else if (result.compareTo(BigInteger.valueOf(Integer.MIN_VALUE)) < 0) {
+            return Integer.MIN_VALUE;
+        }
+
+        return result.intValue();
+    }
+}
+
