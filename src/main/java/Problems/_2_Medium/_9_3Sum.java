45176edbc9e8677e29b38a51c22a871b48896951
stoiandelev <stoian.delev@yahoo.com>
2023-08-26 19:52:39 +0300

    Array operation - two pointer approach

---
+++ src/main/java/Problems/_2_Medium/_9_3Sum.java
@@ -0,0 +1,48 @@
+package Problems._2_Medium;
+
+import java.util.*;
+
+/**
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+ * such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ * Notice that the solution set must not contain duplicate triplets.
+ * Set is used to prevent duplicate triplets, and parallelly
+ * we will use two pointer approach to maintain J and k.
+ */
+
+public class _9_3Sum {
+    public static void main(String[] args) {
+
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+
+        System.out.println(threeSum(nums));
+
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        int target = 0;
+        Arrays.sort(nums);
+        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        output.addAll(s);
+        return output;
+    }
+}
