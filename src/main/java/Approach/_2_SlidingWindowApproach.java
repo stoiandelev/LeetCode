af461a94cddf9161be163059b14719a2c05c2935
stoiandelev <stoian.delev@yahoo.com>
2023-09-06 21:17:19 +0300

    Sliding Window Approach example with explaining.

--- src/main/java/Approach/_2_SlidingWindowApproach.java
+++ src/main/java/Approach/_2_SlidingWindowApproach.java
@@ -1,4 +1,52 @@
 package Approach;
 
+/**
+ * Minimum size Subarray Sum from how many minimum value can do sum grater or
+ * equal given on target.
+ * minimum length;
+ */
+
 public class _2_SlidingWindowApproach {
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 3, 1, 2, 4, 3};
+//        int[] nums = {1, 4, 4};
+        int target = 7;
+
+        System.out.println(minSubArrayLen(target, nums));
+
+
+    }
+
+    public static int minSubArrayLen(int target, int[] nums) {
+        // base case if the array is empty
+
+        // define pointers
+        int L = 0;
+        int R = 0;
+
+        // min subArray length and sum
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+        int n = nums.length;
+
+        // sliding a window to find min sub array length
+
+        while (R < n) {
+            // add the current element
+            sum += nums[R];
+
+            //contract window
+            while (target <= sum) {
+                min = Math.min(min, R - L + 1);
+                sum -= nums[L];
+                L++;
+            }
+            R++;
+        }
+
+        return min == Integer.MAX_VALUE ? 0 : min;
+
+    }
 }
